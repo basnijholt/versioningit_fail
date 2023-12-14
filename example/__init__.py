103b126c6a55afd97a6bd987ddbbbce27d35b099
Bas Nijholt <bas@nijho.lt>
2023-12-14 12:28:55 -0800

    Simplify

--- example/__init__.py
+++ example/__init__.py
@@ -1,5 +1,3 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-
 from ._version import __version__
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# Copyright (c) Microsoft Corporation. All rights reserved.
 
 from setuptools import setup
 from versioningit import get_cmdclasses
